0d5e1ae588946d7a11fcb6efe0ec9bfdc37749ee
Hiroki Homma <homma@maam.me>
2017-09-28 15:23:20 +0900

    mShare line追加

--- src/babel/libs/jquery.mShare.js
+++ src/babel/libs/jquery.mShare.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery mShare v2.0
+ * jQuery mShare v2.1
  * Copyright 2017 maam.inc
  * Contributing Author: Hiroki Homma
  * Require for jQuery v1.7 or above
@@ -11,7 +11,22 @@
           text: $('title').eq(0).text(),
           url: location.origin + location.pathname,
         },
-        $self = this;
+        $self = this,
+        ua = navigator.userAgent,
+        mode = 'pc';
+
+    // sp
+    if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+      mode = 'sp';
+
+    // tab
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+      mode = 'tablet';
+
+    // pc
+    } else {
+      mode = 'pc';
+    }
 
     $self.each(function(){
       var $self = $(this),
@@ -39,17 +54,13 @@
         break;
 
         case 'line':
-          apiUrl = "http://line.me/R/msg/text/";
+          apiUrl = 'http://line.me/R/msg/text/';
 
-          // pc
-          // <li class="item">
-          //                 <a href="https://lineit.line.me/share/ui?url=http%3A%2F%2Fwww.dot-st.com%2Fcp%2Fstudioclip%2Fhamaji_2016win%2F" onclick="window.open(this.href, 'twwindow', 'width=550, height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=1'); return false;" class="mo_op" name="lk_48"><img src="/static/docs/studioclip/pages/201611hamaji_2016win/resource/image/line.svg" alt=""></a>
-          //             </li>
-
-          // sp
-          // <li class="item">
-          //                 <a href="line://msg/text/studio%20CLIP%20featuring%20naoko%20hamajima%20WINTER%202016%20%7C%20%5B%E5%85%AC%E5%BC%8F%5D%E3%82%B9%E3%82%BF%E3%83%87%E3%82%A3%E3%82%AA%E3%82%AF%E3%83%AA%E3%83%83%E3%83%97%EF%BC%88studio%20CLIP%EF%BC%89%E9%80%9A%E8%B2%A9%0d%0ahttp%3A%2F%2Fwww.dot-st.com%2Fcp%2Fstudioclip%2Fhamaji_2016win%2F"><img src="/m/static/docs/studioclip/pages/201611hamaji_2016win/resource/image/line.svg" alt=""></a>
-          //             </li>
+          if(mode === 'pc') {
+            apiUrl = 'https://lineit.line.me/share/ui?url=' + encodeURI(params.url);
+          } else {
+            apiUrl = 'line://msg/text/' + encodeURI(params.text) + '%0d%0a' + encodeURI(params.url);
+          }
         break;
       }
 
